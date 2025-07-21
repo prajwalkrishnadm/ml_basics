8c14bde47a61256d37b6fd717939540ac3655f05
PRAJWAL KRISHNA D M <prajwalkrishnadm@gmail.com>
2025-07-21 19:57:53 +0530

    Create gazetteers.py

---
+++ gazetteers.py
@@ -0,0 +1,4 @@
+
+
+PERSON_NAMES = {"Barack", "Obama", "Narendra", "Modi", "Elon", "Musk", "Prajwal", "Krishna"}
+LOCATIONS = {"Paris", "London", "India", "France", "New York", "Delhi", "Bengaluru"}
